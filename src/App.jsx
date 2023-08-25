da9bf2d6dacaf5455b57427f53c122a6bcde9f1a
nabanit <nabanitkoirala@gmail.com>
2023-08-25 07:24:10 +0545

    updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { ChatIcon } from './assets/svg'
+function App() {
+
+
+  return (
+    <>
+      <h2>Welcome to front end</h2>
+      <ChatIcon />
+    </>
+  )
+}
+
+export default App
