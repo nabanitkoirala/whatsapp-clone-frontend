768b16494af923224f296407e47236d5bbeb004c
nabanit <nabanitkoirala@gmail.com>
2023-08-25 22:13:39 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CallIcon, ChatIcon, DocumentIcon } from './assets/svg'
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
+import { useSelector } from 'react-redux';
+
 function App() {
 
 
+  const user = useSelector((state) => state.user)
+  console.log("user", user)
+
   return (
     <div className='dark' >
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
 
-      <CallIcon className="fill-blue-600" />
     </div>
   )
 }
