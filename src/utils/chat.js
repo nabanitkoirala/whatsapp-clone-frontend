fcddcfebaa7e0a08355e0389c30d2eaa056ec1ef
nabanit <nabanitkoirala@gmail.com>
2023-09-03 12:22:26 +0545

    updated

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -5,7 +5,7 @@ export const getConversationName = (user, users) => {
   return users[0]._id === user._id ? users[1].name : users[0].name;
 };
 export const getConversationPicture = (user, users) => {
-  return users[0]._id === user._id ? users[1].picture : users[0].picture;
+  return users[0]._id === user._id ? users[1].picture.imageUrl : users[0].picture.imageUrl;
 };
 
 export const checkOnlineStatus = (onlineUsers, user, users) => {
